cce4fc979f6f396b012dcca99c972d388b73d4f2
JAgusOckier <juanockier@gmail.com>
2025-03-24 14:32:00 -0300

    feature/ConfirmDialog: implement a confirmDialog reutilizable && improvement: protections routes

--- web/src/middleware.ts
+++ web/src/middleware.ts
@@ -8,10 +8,13 @@ export function middleware(req: NextRequest) {
 
   const blockIfLogued = ["/login", "/register"];
 
-  if (!token && req.nextUrl.pathname.startsWith(protectedRoutes[0])) {
-      const loginUrl = new URL("/login", req.url);
-      return NextResponse.redirect(loginUrl);
-    }
+  const isProtectedRoute = protectedRoutes.some(route =>
+    req.nextUrl.pathname.startsWith(route)
+  );
+
+  if (!token && isProtectedRoute) {
+    return NextResponse.redirect(new URL("/login", req.url));
+  }
 
 
   if (blockIfLogued.includes(req.nextUrl.pathname) && token) {
