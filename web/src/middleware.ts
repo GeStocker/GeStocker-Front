dbe053a20d762ea0fc8884f7a1111c05a05fb913
JAgusOckier <juanockier@gmail.com>
2025-03-28 13:16:12 -0300

    fix/deployGoogle: que no bloquee la ruta si tiene el token por parametros

--- web/src/middleware.ts
+++ web/src/middleware.ts
@@ -5,25 +5,26 @@ export function middleware(req: NextRequest) {
   const token = req.cookies.get("token")?.value;
   
   const protectedRoutes = ["/dashboard"];
-
   const blockIfLogued = ["/login", "/register"];
 
+  const url = req.nextUrl;
+  const hasTokenInURL = url.searchParams.has("token");
+
   const isProtectedRoute = protectedRoutes.some(route =>
-    req.nextUrl.pathname.startsWith(route)
+    url.pathname.startsWith(route)
   );
 
-  if (!token && isProtectedRoute) {
+  if (!token && isProtectedRoute && !hasTokenInURL) { 
     return NextResponse.redirect(new URL("/login", req.url));
   }
 
-
-  if (blockIfLogued.includes(req.nextUrl.pathname) && token) {
-    const loginUrl = new URL("/dashboard/perfil", req.url);
-    return NextResponse.redirect(loginUrl);
+  if (blockIfLogued.includes(url.pathname) && token) {
+    return NextResponse.redirect(new URL("/dashboard/perfil", req.url));
   }
 
   return NextResponse.next();
 }
+
 
 export const config = {
   matcher: ["/dashboard/:path*", "/login", "/register"],
