63020bd3790c500249293a9286bf885df9a0b1f7
JAgusOckier <juanockier@gmail.com>
2025-04-07 14:10:48 -0300

    feature/basicStatistics: implements renderizado by rol

---
+++ web/src/views/Dashboard/Statistics/Charts/AverageSales/AverageSales.tsx
@@ -0,0 +1,183 @@
+import React, { useEffect, useState } from "react";
+import { getAverageSales } from "@/services/user/metrics";
+import { toast } from "sonner";
+import { IAverageSales } from "../../Types";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Button } from "@/components/ui/button";
+import { getAllCategories } from "@/services/user/category";
+import { ICategory } from "@/types/interface";
+import ProductsPieChart from "./ProductsPieChart/ProductsPieChart";
+
+const AverageSales: React.FC<{
+  token: string | null;
+  businessId: string | null;
+}> = ({ token, businessId }) => {
+  const [loading, setLoading] = useState(true);
+  const [selectedInventory, setSelectedInventory] = useState<IAverageSales>();
+  const [averageSales, setAverageSales] = useState<IAverageSales[]>([]);
+  const [selectedCategory, setSelectedCategory] = useState<ICategory | null>(
+    null
+  );
+  const [categories, setCategories] = useState<ICategory[]>([]);
+  const [sortBy, setSortBy] = useState<"daily"|"monthly">("daily")
+
+  const fetchAverageSales = async () => {
+    if (!token || !businessId) return;
+    try {
+      const averageSales = await getAverageSales(
+        token,
+        businessId,
+        sortBy,
+        selectedCategory?.id ?? undefined,
+      );
+      console.log(averageSales)
+      if (averageSales) setAverageSales(averageSales);
+    } catch (e: unknown) {
+      if (e instanceof Error) {
+        console.warn("Error al traer las ventas promedio", e.message);
+        toast.error(`Error: ${e.message}`);
+      } else {
+        console.warn("Error al traer las ventas promedio", e);
+        toast.error("Error al traer las ventas promedio");
+      }
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const fetchCategories = async () => {
+    if (!token) return;
+    try {
+      if (!businessId) return;
+      const categories = await getAllCategories(businessId, token);
+      setCategories(categories);
+    } catch (e: unknown) {
+      if (e instanceof Error) {
+        console.warn("Error al traer las categorias:", e.message);
+
+        toast.error(`Error: ${e.message}`);
+      } else {
+        console.warn("Error al traer las categorias:", e);
+        toast.error("Error al traer las categorias");
+      }
+    }
+  };
+
+  useEffect(() => {
+    fetchCategories();
+    fetchAverageSales();
+  }, [token, businessId, selectedCategory, sortBy]);
+
+  useEffect(() => {
+      if (averageSales.length === 0) return;
+  
+      const foundInventory = averageSales.find(
+        (inv) => inv.inventoryId === selectedInventory?.inventoryId
+      );
+  
+      if (foundInventory) {
+        setSelectedInventory(foundInventory);
+      } else {
+        // Si no está más (por filtro de categoría, por ejemplo), seleccionamos el primero
+        setSelectedInventory(averageSales[0]);
+      }
+    }, [averageSales]);
+
+  return (<div className=" p-4 border rounded-md my-1 bg-custom-grisClarito">
+    <div className="flex justify-between mb-2">
+      <div>
+        <h2 className="text-xl  font-bold">
+          Productos Mayor y Menor Margen de Ganancia
+        </h2>
+        <h3 className="text-lg text-custom-textGris">
+          Los productos con mayor margen de ganancia
+        </h3>
+      </div>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button variant="outline">
+            {selectedInventory?.inventoryName}
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent>
+          {averageSales.map((inventory) => (
+            <DropdownMenuItem
+              key={inventory.inventoryId}
+              onClick={() => setSelectedInventory(inventory)}
+              className={
+                inventory.inventoryId === selectedInventory?.inventoryId
+                  ? "font-bold bg-accent"
+                  : ""
+              }
+            >
+              {inventory.inventoryName}
+            </DropdownMenuItem>
+          ))}
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </div>
+    <div className="mb-4 flex items-center gap-1">
+      <DropdownMenu>
+        <span>Categoria: </span>
+        <DropdownMenuTrigger asChild>
+          <Button variant="outline">
+            {selectedCategory?.name ?? "Todas"}
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuItem onClick={() => setSelectedCategory(null)}>
+            Todas
+          </DropdownMenuItem>
+          {categories.map((categoria) => (
+            <DropdownMenuItem
+              key={categoria.id}
+              onClick={() => setSelectedCategory(categoria)}
+              className={
+                categoria.id === selectedCategory?.id
+                  ? "font-bold bg-accent"
+                  : ""
+              }
+            >
+              {categoria.name}
+            </DropdownMenuItem>
+          ))}
+        </DropdownMenuContent>
+      </DropdownMenu>
+      <DropdownMenu>
+          <span className="ml-2">Ordenar por: </span>
+          <DropdownMenuTrigger asChild>
+            <Button variant="outline">
+              {sortBy === "daily" ? "Diario" : "Mensual"}
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuItem onClick={() => setSortBy("daily")}>
+              Diario
+            </DropdownMenuItem>
+            <DropdownMenuItem onClick={() => setSortBy("monthly")}>
+              Mensual
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+    </div>
+    {loading || !selectedInventory ? (
+      <span className="text-center">Cargando metricas...</span>
+    ) : (
+      <>
+        <ProductsPieChart products={selectedInventory?.topHighAvgSales ?? []} />
+        <h3 className="text-lg text-custom-textGris">
+          Los productos con menor margen de ganancia
+        </h3>
+        <ProductsPieChart products={selectedInventory?.topLowAvgSales ?? []} />
+      </>
+    )}
+  </div>)
+};
+
+
+export default AverageSales
--- web/src/views/Dashboard/Statistics/StatisticsView.tsx
+++ web/src/views/Dashboard/Statistics/StatisticsView.tsx
@@ -5,6 +5,8 @@ import { useBusiness } from "@/context/BusinessContext";
 import { useEffect } from "react";
 import LowStock from "./Charts/LowStock";
 import WithoutSales from "./Charts/WithoutSales";
+import ProfitMargin from "./Charts/MarginProfit/ProfitMargin";
+import AverageSales from "./Charts/AverageSales/AverageSales";
 
 export const months = [
   "Enero",
@@ -22,106 +24,39 @@ export const months = [
 ];
 
 const StatisticsView = () => {
-  const { token } = useAuth();
+  const { token, getUserRol } = useAuth();
   const { businessId } = useBusiness();
+  const rol = getUserRol();
 
-  
-  useEffect(() => {
-    }, [token, businessId]);
+  useEffect(() => {}, [token, businessId]);
   return (
     <div className="p-4">
-      <h1 className="text-4xl font-bold">Mi perfil</h1>
+      <h1 className="text-4xl font-bold">Panel de Estadísticas</h1>
       <h2 className="text-xl text-custom-textSubtitle">
-        Administra tu información personal y preferencias
+        Visualiza las métricas y estadísticas de tu negocio
       </h2>
-      <section className="w-full p-4 grid grid-cols-2 gap-4">
-        <MonthlyProfit token={token} businessId ={businessId}/>
-        <LowStock token={token} businessId ={businessId}/>
-        <WithoutSales token={token} businessId ={businessId}/>
-      </section>
-      <section className="w-full p-4 grid grid-cols-2 gap-4">
-
-      </section>
-      {/* <section className="w-full p-4 grid grid-cols-2 gap-4">
-        <div className=" p-4 border rounded-md my-8">
-          <h2 className="text-xl  font-bold">
-            Top 5 Productos Más Vendidos
-          </h2>
-          <h3 className="text-sm text-custom-textGris">
-            Productos con mayor volumen de ventas
-          </h3>
-          <ChartContainer
-            config={chartConfig2}
-            className="mx-auto aspect-square max-h-[300px]"
-          >
-            <PieChart>
-              <ChartTooltip 
-                content={<ChartTooltipContent nameKey="ventas" hideLabel/>}
-              />
-              <Pie
-                data={chartData}
-                dataKey="ventas"
-                label={({ payload, ...props }) => {
-                  return (
-                    <text
-                      cx={props.cx}
-                      cy={props.cy}
-                      x={props.x}
-                      y={props.y}
-                      textAnchor={props.textAnchor}
-                      dominantBaseline={props.dominantBaseline}
-                      fill="var(--foreground)"
-                    >
-                      {payload.ventas}
-                    </text>
-                  );
-                }}
-                nameKey="mueble"
-              />
-              <ChartLegend
-                content={<ChartLegendContent nameKey="mueble" />}
-                className="-translate-y-2 flex-wrap gap-2 [&>*]:basis-1/4 [&>*]:justify-center"
-              />
-            </PieChart>
-          </ChartContainer>
-        </div> */}
-      {/*  */}
-
-      {/* <div className=" p-4 border rounded-md my-8">
-          <h2 className="text-xl  font-bold">
-            Tasa de Pérdida de Productos
-          </h2>
-          <h3 className="text-sm text-custom-textGris">
-            Porcentaje de productos perdidos o dañados
-          </h3>
-        </div>
-        <div className=" p-4 border rounded-md my-8">
-          <h2 className="text-xl  font-bold">
-            Productos Más Perdidos
-          </h2>
-          <h3 className="text-sm text-custom-textGris">
-            Productos con mayor tasa de pérdida
-          </h3>
-        </div> */}
-      {/* </section> */}
-      {/* <section className="w-full p-4 flex gap-4">
-        <div className=" p-4 border rounded-md my-8">
-          <h2 className="text-xl  font-bold">
-            Tiempo Hasta Acabar Stock
-          </h2>
-          <h3 className="text-sm text-custom-textGris">
-            Estimación basada en la tasa de ventas actual
-          </h3>
-        </div>
-        <div className=" p-4 border rounded-md my-8">
-          <h2 className="text-xl  font-bold">
-            Comparación de Rendimiento Mensual
-          </h2>
-          <h3 className="text-sm text-custom-textGris">
-            Ingresos, gastos y ganancias por mes
-          </h3>
-        </div>
-      </section> */}
+      {!rol ? (
+        <span>Cargando metricas</span>
+      ) : (
+        <section className="w-full p-4 grid grid-cols-2 gap-4 items-start">
+          {(rol === "basic" ||
+            rol === "professional" ||
+            rol === "business") && (
+            <>
+              <LowStock token={token} businessId={businessId} />
+              <WithoutSales token={token} businessId={businessId} />
+              <MonthlyProfit token={token} businessId={businessId} />
+            </>
+          )}
+          {(
+            rol === "professional" ||
+            rol === "business") && (
+            <>
+          <ProfitMargin token={token} businessId={businessId} />
+          <AverageSales token={token} businessId={businessId} />
+          </>)}
+        </section>
+      )}
     </div>
   );
 };
