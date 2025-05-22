4cb826ee3d566ebbb3cf2c10a60bc7df0752da89
JAgusOckier <juanockier@gmail.com>
2025-05-22 17:46:25 -0300

    fix/inventoryName

--- web/src/views/Dashboard/inventory/InventoryView.tsx
+++ web/src/views/Dashboard/inventory/InventoryView.tsx
@@ -11,9 +11,10 @@ import { toast } from "sonner";
 import { IStockProduct } from "@/types/interface";
 import Link from "next/link";
 import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@/components/ui/tooltip";
+import { getAllInventory } from "@/services/user/inventory";
 
 const InventoryView = () => {
-  const { inventoryId, inventories} = useBusiness();
+  const { inventoryId, inventories, setInventories} = useBusiness();
   const { token } = useAuth();
   const [products, setProducts] = useState<IStockProduct[]>([]);
   const [filters, setFilters] = useState({
@@ -46,6 +47,7 @@ const InventoryView = () => {
     };
 
     fetchProductsInventory();
+    fetchInventories()
   }, [inventoryId, token, filters, businessId]);
 
   const handleSearchChange = (value: string): void => {
@@ -53,6 +55,16 @@ const InventoryView = () => {
       };
     
   const currentInventory = inventories.find(inv => inv.id === inventoryId) || inventories[0];
+
+  const fetchInventories = async () => {
+      if (!token || !businessId) return;
+      try {
+        const inventories = await getAllInventory(token, businessId);
+        setInventories(inventories);
+      } catch (error) {
+        console.warn("No se pudo traer los Locales", error);
+      }
+    };
 
   const totalProducts = products.length;
   const outOfStockProducts = products.filter((p) => p.stock <= 0).length;
